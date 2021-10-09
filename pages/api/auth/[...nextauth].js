f42c771be3277a27c68f97b29a554f55702dbd55
pawor <pawor.emi@gmail.com>
2021-10-09 14:22:40 +0700

    next-auth-demo

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+    session:{
+        jwt:true
+    },
+    providers:[
+        Providers.Google({
+            clientId:process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_SECRET
+        }),
+        Providers.Facebook({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+        }),
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        })
+    ],
+    pages:{
+        signIn:'/login'
+    },
+    //MongoDB database
+    database:process.env.DATABASE_URL,
+})
